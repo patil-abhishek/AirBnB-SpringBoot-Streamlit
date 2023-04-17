bb4e0f16a0b9aef3d68dd9169cf1d27230b6f4e6
Anant <anantgulati.3@gmail.com>
2023-04-17 12:36:05 +0530

    bug fixes

--- springboot-api/src/main/java/pes/ooad/airbnb/controller/ChartController.java
+++ springboot-api/src/main/java/pes/ooad/airbnb/controller/ChartController.java
@@ -1,3 +1,5 @@
+package pes.ooad.airbnb.controller;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
--- springboot-api/src/main/java/pes/ooad/airbnb/controller/CustomerConntroller.java
+++ springboot-api/src/main/java/pes/ooad/airbnb/controller/CustomerConntroller.java
@@ -1,9 +1,22 @@
+package pes.ooad.airbnb.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
 public class CustomerController {
 
+    @Autowired
+    private ChartJs chartJs;
+
     @GetMapping("/customer-age-data")
     public List<CustomerAgeData> getCustomerAgeData() {
-        // TODO: retrieve customer data from database or service and calculate the number of customers in each age bracket
+        // TODO: retrieve customer data from database or service
+        // and calculate the number of customers in each age bracket
         List<CustomerAgeData> data = new ArrayList<>();
         data.add(new CustomerAgeData("Under 18", 100));
         data.add(new CustomerAgeData("18-24", 200));
@@ -11,24 +24,9 @@ public class CustomerController {
         data.add(new CustomerAgeData("35-44", 250));
         data.add(new CustomerAgeData("45-54", 150));
         data.add(new CustomerAgeData("Over 54", 100));
+
+        chartJs.createPieChart("customerAgeChart", "Customer Age Data", data);
+
         return data;
-    }
-}
-
-public class CustomerAgeData {
-    private String ageBracket;
-    private int count;
-
-    public CustomerAgeData(String ageBracket, int count) {
-        this.ageBracket = ageBracket;
-        this.count = count;
-    }
-
-    public String getAgeBracket() {
-        return ageBracket;
-    }
-
-    public int getCount() {
-        return count;
     }
 }
